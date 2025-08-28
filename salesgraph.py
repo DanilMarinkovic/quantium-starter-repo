aa5a79959cdef26753a0a3a142eaea3d22b4e4ed
Danil Marinkovic <danilmarinkovic@icloud.com>
2025-08-28 22:30:58 +0100

    radio and basic styling

--- salesgraph.py
+++ salesgraph.py
@@ -1,26 +1,51 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
 app = Dash()
 df=pd.read_csv("output.csv")
-df["sales"] = df["sales"].str.replace(r'\$', '', regex=True).astype(float)
+df["sales"] = df["sales"].str.replace(r"\$", "", regex=True).astype(float)
 df["date"] = pd.to_datetime(df["date"])
 
 fig = px.line(df, x="date", y="sales", color="region")
 
-app.layout = html.Div(children=[
-    html.H1(children='Sales of Pink Morsel Over Time'),
+app.layout = html.Div(
+    style={
+        "backgroundColor": "#FFFFFF",
+        "color": "#2335FA",
+    },
+    children=[
+    html.H1(children="Sales of Pink Morsel Over Time", style={"textAlign": "center"}),
 
-    html.Div(children='''
-        This graph shows the sales of "pink morsel" over time across all regions. As seen, after the price increase on the 15th of January 2021, all regions had an increase in sales.
-    '''),
+    html.Div(children="This graph shows the sales of pink morsel over time across different regions. As seen, after the price increase on the 15th of January 2021, all regions had an increase in sales."),
 
     dcc.Graph(
-        id='Sales Graph',
-        figure=fig
+        id="Sales Graph",
+        figure=fig,
+        style={"backgroundColor": "#000000"}
+    ),
+    dcc.RadioItems(
+        options=["north", "south", "east", "west", "all"],
+        value="all",
+        id="region-selector",
+        style={"textAlign": "center"},
+        labelStyle={"display": "inline-block"}
+
     )
 ])
 
-if __name__ == '__main__':
+@app.callback(
+    Output("Sales Graph", "figure"),
+    Input("region-selector", "value")
+)
+def update_graph(selected_region):
+    if selected_region == "all":
+        filtered_df = df
+    else:
+        filtered_df = df[df["region"] == selected_region]
+    
+    fig = px.line(filtered_df, x="date", y="sales", color="region")
+    return fig
+
+if __name__ == "__main__":
     app.run(debug=True)
